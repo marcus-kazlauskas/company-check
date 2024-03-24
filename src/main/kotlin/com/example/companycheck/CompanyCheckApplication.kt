01166ebd19f4ddff575debdf386c69e076e30478
KozlovMV <KozlovMV@kadastr.ru>
2024-03-24 16:09:17 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/companycheck/CompanyCheckApplication.kt
@@ -0,0 +1,11 @@
+package com.example.companycheck
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CompanyCheckApplication
+
+fun main(args: Array<String>) {
+	runApplication<CompanyCheckApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/companycheck/CompanyCheckApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.companycheck
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CompanyCheckApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
